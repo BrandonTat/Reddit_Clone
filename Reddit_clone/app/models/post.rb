00a0edfdf8f0772518f165467c552aa621300126
Brandon Tat <brandontat7@gmail.com>
2017-04-24 21:48:23 -0700

    Reddit clone

---
+++ Reddit_clone/app/models/post.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  url        :string
+#  content    :text
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ApplicationRecord
+  validates :title, :author, presence: true
+
+  belongs_to :author,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :user_id
+end
